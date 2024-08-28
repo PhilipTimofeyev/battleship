24ca5b368efa6dd842c683ec80c35b443c4caf56
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-08-28 16:43:36 -0700

    Set up carrier ship

--- src/ship.js
+++ src/ship.js
@@ -1,1 +1,44 @@
 
+class Ship {
+	constructor(length) {
+		this.length = length
+		this.hits = 0;
+		this.sunk = false;
+	}
+
+	hit() {
+		this.hits++
+	}
+
+	isSunk() {
+		this.sunk = this.hits >= this.length
+		return this.sunk
+	}
+}
+
+
+class Carrier extends Ship {
+	constructor() {
+		super(5)
+	}
+}
+
+function sums(a, b) {
+  return a + b;
+}
+
+module.exports = sums;
+
+
+
+// let hmm = new Carrier
+
+// hmm.hit()
+// hmm.hit()
+// hmm.hit()
+// hmm.hit()
+// hmm.hit()
+
+// console.log(hmm)
+// console.log(hmm.isSunk())
+// console.log(hmm)
---
+++ src/test/ship.test.js
@@ -0,0 +1,16 @@
+
+const sums = require('../ship');
+
+// const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sums(1, 2)).toBe(3);
+});
+
+// beforeEach(() => {
+//   const carrier = new Carrier;
+// });
+
+// test('carrier length', () => {
+//   expect(carrier.length.toBe(5));
+// });
