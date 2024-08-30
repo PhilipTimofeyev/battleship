b40745db1ded3dc6d29c8bd9a681abc6c935ef70
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-08-30 07:55:58 -0700

    Refactor board set up functions and set up player 2 board

--- src/main.js
+++ src/main.js
@@ -5,38 +5,52 @@ const player1 = new Player
 const player2 = new Player
 
 // DOM Elements
-const boardContainer = document.querySelector(".board")
+const player1Board = document.querySelector(".player1")
+const player2Board = document.querySelector(".player2")
 
 // Create Board
-
-function addSquare(coord, square) {
+function createSquare(coord, square) {
   const newDiv = document.createElement("div");
   const newContent = document.createTextNode(coord);
 
   newDiv.classList.add('square')
   newDiv.appendChild(newContent);
-  newDiv.dataset.coordinate = coord
-  boardContainer.appendChild(newDiv);
+  newDiv.dataset.coordinate = coord;
+  return newDiv
 }
 
+function addSquare(square, board) {
+  board.appendChild(square)
+}
 
-Object.entries(player1.gameboard.board).forEach(([coord, square]) => {
-	addSquare(coord, square)
-})
+function setUpBoard(player, board) {
+	Object.entries(player.gameboard.board).forEach(([coord, square]) => {
+		const newSquare = createSquare(coord, square)
+		addSquare(newSquare, board)
+	})
+}
 
+// Update Board
 
 function updateBoard() {
-	Object.entries(player1.gameboard.board).forEach(([coord, square]) => {
-		const coordElement = boardContainer.querySelector(`[data-coordinate=${coord}]`)
+	updatePlayerBoard(player1, player1Board);
+	updatePlayerBoard(player2, player2Board);
+
+}
+
+function updatePlayerBoard(player, board) {
+	Object.entries(player.gameboard.board).forEach(([coord, square]) => {
+		const coordElement = board.querySelector(`[data-coordinate=${coord}]`)
 		coordElement.innerText = square.ship ? square.ship.name : coord
 	})
 }
-updateBoard()
+
+setUpBoard(player1, player1Board)
+setUpBoard(player2, player2Board)
 
 const carrier = new Carrier
 
-console.log(carrier.name)
-
-player1.gameboard.placeShip(carrier, "A2", "A8")
+player1.gameboard.placeShip(carrier, "A2", "A5")
+player2.gameboard.placeShip(carrier, "A2", "A5")
 
 updateBoard()
