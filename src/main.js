a8a07307b71c1cead20eea80d730a2784aa5348a
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-08-29 20:07:36 -0700

    Create updateBoard function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { Player } from './player';
+import { Carrier, Battleship, Destroyer, Submarine, Patrol } from './ship';
 
 const player1 = new Player
 const player2 = new Player
@@ -8,17 +9,33 @@ const boardContainer = document.querySelector(".board")
 
 // Create Board
 
-function addSquare(name) {
+function addSquare(coord, square) {
   const newDiv = document.createElement("div");
-  const newContent = document.createTextNode(name);
+  const newContent = document.createTextNode(coord);
 
   newDiv.classList.add('square')
   newDiv.appendChild(newContent);
-  newDiv.dataset.coordinate = name
+  newDiv.dataset.coordinate = coord
   boardContainer.appendChild(newDiv);
 }
 
 
-Object.entries(player1.gameboard.board).forEach(([k, v]) => {
-	addSquare(k)
+Object.entries(player1.gameboard.board).forEach(([coord, square]) => {
+	addSquare(coord, square)
 })
+
+
+function updateBoard() {
+	Object.entries(player1.gameboard.board).forEach(([coord, square]) => {
+		const coordElement = boardContainer.querySelector(`[data-coordinate=${coord}]`)
+		coordElement.innerText = square.ship
+	})
+}
+
+updateBoard()
+
+const carrier = new Carrier
+
+player1.gameboard.placeShip(carrier, "A1", "C1")
+
+updateBoard()
