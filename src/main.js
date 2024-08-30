fabb6a934324408bb84634df8be625024085790b
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-08-29 18:43:09 -0700

    Create board in dom with dataset attribute associated with coordinate

--- src/main.js
+++ src/main.js
@@ -9,24 +9,16 @@ const boardContainer = document.querySelector(".board")
 // Create Board
 
 function addSquare(name) {
-  // create a new div element
   const newDiv = document.createElement("div");
-
-  // and give it some content
   const newContent = document.createTextNode(name);
+
   newDiv.classList.add('square')
-
-  // add the text node to the newly created div
   newDiv.appendChild(newContent);
-
-  // add the newly created element and its content into the DOM
-  // const currentDiv = document.getElementById("div1");
-  boardContainer.appendChild(newContent);
+  newDiv.dataset.coordinate = name
+  boardContainer.appendChild(newDiv);
 }
 
 
 Object.entries(player1.gameboard.board).forEach(([k, v]) => {
 	addSquare(k)
 })
-
-// addElement()
