1b5380aaf4990b4d33510fbadc1820a41eb6f544
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-09-01 07:33:45 -0700

    Add tests for computer to determine which ship to use

--- src/test/player.test.js
+++ src/test/player.test.js
@@ -1,5 +1,49 @@
+import { Human, Computer } from '../player';
+import { Gameboard } from '../gameboard';
+import { Ship } from '../ship';
 
+let gameboard
+let computer
 
-test('placeholder', () => {
-  expect(true).toBe(true);
+beforeEach(() => {
+  gameboard = new Gameboard;
+  computer = new Computer;
 });
+
+test('Computer uses next biggest ship that is not sunk ', () => {
+  const battleship = Ship.createShip('Battleship')
+  const submarine = Ship.createShip('Submarine')
+
+  battleship.sunk = true
+  submarine.sunk = true
+
+  gameboard.placeShip(battleship, 'A1', 'A2')
+  gameboard.placeShip(submarine, 'B1', 'E2')
+
+  const result = computer.determineShip(gameboard).name
+
+  expect(result).toBe('Carrier')
+});
+
+test('Computer uses next biggest ship that is not sunk ', () => {
+  const carrier = Ship.createShip('Carrier')
+  const battleship = Ship.createShip('Battleship')
+  const submarine = Ship.createShip('Submarine')
+
+  carrier.sunk = true
+  battleship.sunk = true
+  submarine.sunk = true
+
+  gameboard.placeShip(battleship, 'A1', 'A2')
+  gameboard.placeShip(submarine, 'B1', 'E2')
+  gameboard.placeShip(carrier, 'E4', 'E5')
+
+  const result = computer.determineShip(gameboard).name
+
+  expect(result).toBe('Destroyer')
+});
+
+
+
+
+
